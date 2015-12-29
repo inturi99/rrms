573c58a2f02e103f6c2d3bf55d290ebfbfd4c06f
abelia <inturi99@gmail.com>
2015-12-29 17:22:24 +0530

    Implimentation of delete query

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -3,5 +3,12 @@
 SELECT * from documents
 WHERE title = :title
 
+--name: get-documents-by-id
+SELECT documentname,title,employeename,location,barcode,isactive from documents WHERE id = :id
+
 -- name: insert-documents
 INSERT INTO documents(documentname,title,employeename,date,location,barcode,isactive,createdatetime,updateddatetime) VALUES(:documentname,:title,:employeename,CURRENT_DATE,:location,:barcode,:isactive,now(),now())
+
+
+-- name: delete-documents-by-id
+UPDATE documents set isactive = 'FALSE' where id = :id
