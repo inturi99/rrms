e96112ea8beb85e7965cbda5dc2a51f92e73cdd4
abelia <inturi99@gmail.com>
2015-12-29 18:11:44 +0530

    create update documents query

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -9,6 +9,8 @@ SELECT documentname,title,employeename,location,barcode,isactive from documents 
 -- name: insert-documents
 INSERT INTO documents(documentname,title,employeename,date,location,barcode,isactive,createdatetime,updateddatetime) VALUES(:documentname,:title,:employeename,CURRENT_DATE,:location,:barcode,:isactive,now(),now())
 
+--name: update-documents
+UPDATE documents set documentname = :documentname,title = :title,employeename = :employeename,date = CURRENT_DATE,location = :location,isactive = :isactive,updateddatetime = now()  where id = :id
 
 -- name: delete-documents-by-id
 UPDATE documents set isactive = 'FALSE' where id = :id
