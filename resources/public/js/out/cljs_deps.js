b0bc8db3bd26a2cd4dcc2b93bac61155839b3c19
INTURI <inturi99@gmail.com>
2016-01-01 00:51:29 +0530

    Remove resources/public/js  folder

--- resources/public/js/out/cljs_deps.js
+++
@@ -1,3 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
-goog.addDependency("../rrms/core.js", ['rrms.core'], ['cljs.core']);
--- resources/public/js/out/rrms/core.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2843 {}
-goog.provide('rrms.core');
-goog.require('cljs.core');
-
-//# sourceMappingURL=core.js.map
