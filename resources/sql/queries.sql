19f1bb6cff42d1209286a1341d2f98ed2de4d624
abelia <inturi99@gmail.com>
2015-12-31 17:57:31 +0530

    update query.sql

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -4,16 +4,19 @@ SELECT * from documents
 WHERE title = :title
 
 --name: get-all-documents
-SELECT documentname,title,employeename,date,location,isactive from documents ORDER BY createdatetime DESC
+SELECT id,documentname,title,employeename,date,location,isactive from documents WHERE isactive = 'TRUE' ORDER BY createdatetime DESC
 
 --name: get-documents-by-id
 SELECT documentname,title,employeename,location,barcode,isactive from documents WHERE id = :id
 
 -- name: insert-documents
-INSERT INTO documents(documentname,title,employeename,date,location,barcode,isactive,createdatetime,updateddatetime) VALUES(:documentname,:title,:employeename,CURRENT_DATE,:location,:barcode,:isactive,now(),now())
+INSERT INTO documents(documentname,title,employeename,date,location,barcode,isactive,createdatetime,updateddatetime) VALUES(:documentname,:title,:employeename,:date,:location,:barcode,'TRUE',now(),now())
+RETURNING id,documentname,title,employeename,date,location,barcode,isactive
 
 --name: update-documents
-UPDATE documents set documentname = :documentname,title = :title,employeename = :employeename,date = CURRENT_DATE,location = :location,isactive = :isactive,updateddatetime = now()  where id = :id
+UPDATE documents set documentname = :documentname,title = :title,employeename = :employeename,date = CURRENT_DATE,location = :location,updateddatetime = now()  where id = :id
+RETURNING id,documentname,title,employeename,date,location,barcode,isactive
 
 -- name: delete-documents-by-id
 UPDATE documents set isactive = 'FALSE' where id = :id
+RETURNING id
