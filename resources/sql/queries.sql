09b41ded8520b4a5330635951b86ca538001205b
INTURI <inturi99@gmail.com>
2016-01-01 23:49:52 +0530

    Update documents query add date

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -7,14 +7,14 @@ WHERE title = :title
 SELECT id,documentname,title,employeename,date,location,isactive from documents WHERE isactive = 'TRUE' ORDER BY createdatetime DESC
 
 --name: get-documents-by-id
-SELECT documentname,title,employeename,location,barcode,isactive from documents WHERE id = :id
+SELECT id, documentname,title,employeename,date,location from documents WHERE id = :id
 
 -- name: insert-documents
 INSERT INTO documents(documentname,title,employeename,date,location,barcode,isactive,createdatetime,updateddatetime) VALUES(:documentname,:title,:employeename,:date,:location,:barcode,'TRUE',now(),now())
 RETURNING id,documentname,title,employeename,date,location,barcode,isactive
 
 --name: update-documents
-UPDATE documents set documentname = :documentname,title = :title,employeename = :employeename,date = CURRENT_DATE,location = :location,updateddatetime = now()  where id = :id
+UPDATE documents set documentname = :documentname,title = :title,employeename = :employeename,date = :date,location = :location,updateddatetime = now()  where id = :id
 RETURNING id,documentname,title,employeename,date,location,barcode,isactive
 
 -- name: delete-documents-by-id
