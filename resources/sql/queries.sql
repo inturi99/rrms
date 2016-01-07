a06f4aef0b1eaf9369023d0b34a2a8918fbe1250
abelia <inturi99@gmail.com>
2016-01-07 14:46:30 +0530

    Update get-all-documents-by-index-page size query

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -7,7 +7,7 @@ WHERE lower(title) LIKE ('%' || lower(:title) || '%')
 SELECT id,documentname,title,employeename,date,location,isactive from documents WHERE isactive = 'TRUE' ORDER BY createdatetime DESC
 
 --name: get-all-documents-by-index-pagesize
-SELECT id,documentname,title,employeename,date,location,isactive  FROM (SELECT  ROW_NUMBER() OVER (ORDER BY id asc) as MyRowNumber,* FROM Documents) tblDocuments WHERE MyRowNumber BETWEEN ( ((:index - 1) * :pagesize )+ 1) AND :index*:pagesize
+SELECT MyRowNumber, id,documentname,title,employeename,date,location,isactive  FROM (SELECT  ROW_NUMBER() OVER (ORDER BY id asc) as MyRowNumber,* FROM Documents WHERE isactive = 'TRUE' ) tblDocuments WHERE  MyRowNumber BETWEEN ( ((:index - 1) * :pagesize )+ 1) AND :index*:pagesize
 
 --name: get-documents-by-id
 SELECT id, documentname,title,employeename,date,location from documents WHERE id = :id
