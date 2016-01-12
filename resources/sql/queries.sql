75e927ef641460b0f481b43562578288938f674a
abelia <inturi99@gmail.com>
2016-01-12 17:34:46 +0530

    add get-documents-between-two-dates query

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,7 +1,10 @@
 -- name: get-documents-by-title
 -- search by title
 SELECT * from documents
-WHERE lower(title) LIKE ('%' || lower(:title) || '%')
+WHERE lower(documentname) LIKE ('%' || lower(:documentname) || '%') OR lower(title) LIKE ('%' || lower(:title) || '%')
+
+-- name: get-documents-between-two-dates
+select * from documents where date = :date1 OR  (date BETWEEN :date1  AND :date2) OR lower(documentname) LIKE ('%' || lower(:documentname) || '%') OR lower(title) LIKE ('%' || lower(:title) || '%')  AND isactive = 'TRUE' ORDER BY createdatetime DESC
 
 --name: get-all-documents
 SELECT id,documentname,title,employeename,date,location,isactive from documents WHERE isactive = 'TRUE' ORDER BY createdatetime DESC
@@ -10,8 +13,7 @@ SELECT id,documentname,title,employeename,date,location,isactive from documents 
 SELECT COUNT(id) AS totaldocuments from documents where isactive = 'TRUE'
 
 --name: get-all-documents-by-index-pagesize
-SELECT MyRowNumber, id,documentname,title,employeename,date,location,isactive  FROM (SELECT  ROW_NUMBER() OVER (ORDER BY id asc) as MyRowNumber,* FROM Documents WHERE isactive = 'TRUE' ) tblDocuments WHERE  MyRowNumber BETWEEN ( ((:index - 1) * :pagesize )+ 1) AND :index*:pagesize
-
+SELECT MyRowNumber, id,documentname,title,employeename,date,location,isactive  FROM (SELECT  ROW_NUMBER() OVER (ORDER BY id DESC) as MyRowNumber,* FROM Documents WHERE isactive = 'TRUE') tblDocuments WHERE  MyRowNumber BETWEEN ( ((:index - 1) * :pagesize )+ 1) AND :index*:pagesize
 --name: get-documents-by-id
 SELECT id, documentname,title,employeename,date,location from documents WHERE id = :id
 
